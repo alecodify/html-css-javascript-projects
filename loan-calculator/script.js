b1a4ae9f78880eb6fb1789388468c7fd0d916ded
aliraza <chaudaryaliraza0@gmail.com>
2024-08-31 10:01:32 +0500

    #25 completed

---
+++ loan-calculator/script.js
@@ -0,0 +1,12 @@
+function calculateLoan() {
+    loanAmount = document.getElementById('loan-amount').value;
+    interestRate = document.getElementById('interest-rate').value;
+    MonthsToPay = document.getElementById('pay').value;
+
+    interest = (loanAmount * (interestRate * 0.01)) / MonthsToPay;
+
+    monthlyPayment = (loanAmount / MonthsToPay + interest).toFixed(2);
+
+    document.getElementById('payment').innerHTML = `Monthly Payment: ${monthlyPayment}`;
+
+}
