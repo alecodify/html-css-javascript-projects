b20f83f2f9e58ad9a62a0604a07b313ed09e566e
aliraza <chaudaryaliraza0@gmail.com>
2024-09-03 15:14:03 +0500

    #63 completed

---
+++ sound-board/script.js
@@ -0,0 +1,22 @@
+const buttons = document.getElementById("buttons");
+
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+const stopSounds = () => {
+    sounds.forEach((sound) => {
+        const currentSound = document.getElementById(sound);
+        currentSound.pause();
+        currentSound.currentTime = 0;
+    });
+};
+
+sounds.forEach((sound) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.innerText = sound;
+    btn.addEventListener("click", () => {
+        stopSounds();
+        document.getElementById(sound).play();
+    });
+    buttons.appendChild(btn);
+});
