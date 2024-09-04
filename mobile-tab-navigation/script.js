add20221594cb36e6eda0372b73e7fab580aa6bc
aliraza <chaudaryaliraza0@gmail.com>
2024-09-04 13:06:06 +0500

    #91 completed

---
+++ mobile-tab-navigation/script.js
@@ -0,0 +1,12 @@
+const contents = document.querySelectorAll(".content");
+const listItems = document.querySelectorAll("nav ul li");
+
+listItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    contents.forEach(content => content.classList.remove("show"));
+    listItems.forEach(listItem => listItem.classList.remove("active"));
+
+    item.classList.add("active");
+    contents[index].classList.add("show");
+  });
+});
